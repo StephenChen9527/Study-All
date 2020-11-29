690113da4e9ae03616712a4073efa3025219492f
StephenChen9527 <cundiandongxi1994@gmail.com>
2020-11-29 23:42:41 +0800

    update DUBBO springboot and  hystrix

--- Study-RPC/Study-Dubbo-SpringBoot-Consumer/src/main/java/com/nullbugs/dubbo/consumer/DubboConsumerApplication.java
+++ Study-RPC/Study-Dubbo-SpringBoot-Consumer/src/main/java/com/nullbugs/dubbo/consumer/DubboConsumerApplication.java
@@ -1,11 +1,13 @@
 package com.nullbugs.dubbo.consumer;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.nullbugs.dubbo.Service.IDubboService;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.config.annotation.Method;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableDubbo
 @RestController
+@EnableHystrix
 public class DubboConsumerApplication {
 
     @DubboReference(
@@ -27,12 +30,14 @@ public class DubboConsumerApplication {
     }
 
     @RequestMapping(value = "/dubbo")
+    @HystrixCommand(fallbackMethod = "mock")
     public String test(@RequestParam("id") String id)throws Exception{
         String info = service.getInfo(id);
         System.out.println(info);
         return "success";
     }
-    public String test2(String id)throws Exception{
+
+    public String mock(String id){
         return "error";
     }
 }
